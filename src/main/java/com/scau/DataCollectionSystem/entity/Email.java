26c777faa9d1e2aa7ada6899baaea95f4a4d5d47
ASunnn <liurf923@gmail.com>
2018-01-29 17:39:10 +0800

    恢复

--- src/main/java/com/scau/DataCollectionSystem/entity/Email.java
+++ src/main/java/com/scau/DataCollectionSystem/entity/Email.java
@@ -22,4 +22,35 @@ public class Email {
     @Field
     private boolean enable;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
 }
