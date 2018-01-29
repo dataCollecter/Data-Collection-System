1822326c6f6619b0a5cfb3cfb6c2383047e658e9
jess <493954947@qq.com>
2018-01-29 22:42:57 +0800

    合并配置和管理，新增调用爬虫

--- src/main/java/com/scau/DataCollectionSystem/dao/SpiderDao.java
+++ src/main/java/com/scau/DataCollectionSystem/dao/SpiderDao.java
@@ -1,9 +1,11 @@
 package com.scau.DataCollectionSystem.dao;
 
 import com.scau.DataCollectionSystem.entity.Spider;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by 哲帆 on 2018.1.29.
  */
+@Repository
 public interface SpiderDao extends MongoBase<Spider> {
 }
