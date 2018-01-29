4427e1c776187f778a684c8314908fedbd3ffbed
jess <493954947@qq.com>
2018-01-30 02:16:39 +0800

    stable

--- src/test/java/com/scau/DataCollectionSystem/dao/daoTest.java
+++ src/test/java/com/scau/DataCollectionSystem/dao/daoTest.java
@@ -9,8 +9,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * Created by 哲帆 on 2018.1.29.
@@ -23,9 +26,27 @@ public class daoTest {
     private SpiderDao spiderDao;
     @Test
     public void Test() {
+        spiderDao.remove(new Query(),Spider.class);
         spiderDao.insert(new Spider("a","2017-01-01","1","1","1","1"));
+        spiderDao.insert(new Spider("b","2017-01-01","2","1","1","1"));
+        spiderDao.insert(new Spider("c","2017-01-01","1","3","1","1"));
+        spiderDao.insert(new Spider("d","2017-01-01","1","1","4","1"));
         Spider spider=spiderDao.findOne(new Query(Criteria.where("name").is("a")),Spider.class);
         System.out.println(spider.getName()+spider.getTitle1());
+        List<Spider> spiderList=spiderDao.findAll(Spider.class);
+        System.out.println(spiderList.size());
+        spiderList=spiderDao.find(new Query(),Spider.class);
+        System.out.println(spiderList.size());
+        Update update=new Update();
+        update.set("title1","4 ");
+        spiderDao.updateFirst(new Query(Criteria.where("name").is("a")),update,Spider.class);
+        spider=spiderDao.findOne(new Query(Criteria.where("name").is("a")),Spider.class);
+        System.out.println(spider.getName()+spider.getTitle1());
+        spiderDao.updateMulti(new Query(Criteria.where("url").is("2017-01-01")),update,Spider.class);
+        spiderList=spiderDao.findAll(Spider.class);
+        for (int i=0;i<4;i++){
+            System.out.println(spiderList.get(i).getTitle1());
+        }
     }
 
 }
