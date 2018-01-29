1089e75f874b00ee459221019ec1a0fa57e6d672
jess <493954947@qq.com>
2018-01-29 17:37:27 +0800

    Add spiderconfig service Add spidermanagement service

---
+++ src/main/java/com/scau/DataCollectionSystem/service/SpiderManagementService.java
@@ -0,0 +1,15 @@
+package com.scau.DataCollectionSystem.service;
+
+import java.util.List;
+
+import com.scau.DataCollectionSystem.entity.Spider;
+import com.scau.dataCollectionSystem.entity.spider;
+
+/**
+ * Created by 哲帆 on 2018.1.29.
+ */
+public interface SpiderManagementService {
+
+    List<Spider> getSpiderList();
+
+}
