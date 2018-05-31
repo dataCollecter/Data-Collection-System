b4e003ecb5248949aed1c159b1927ee216821415
jess <493954947@qq.com>
2018-05-31 22:25:25 +0800

    add entity followpath add service remove followpath

---
+++ src/main/java/com/scau/DataCollectionSystem/dao/impl/FollowPathDaoImpl.java
@@ -0,0 +1,20 @@
+package com.scau.DataCollectionSystem.dao.impl;
+
+import com.scau.DataCollectionSystem.dao.FollowPathDao;
+import com.scau.DataCollectionSystem.dao.MongoBase;
+import com.scau.DataCollectionSystem.entity.FollowPath;
+import com.scau.DataCollectionSystem.entity.Spider;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+/**
+ * Created by 哲帆 on 2018.5.31.
+ */
+public class FollowPathDaoImpl extends MongoBaseImpl<FollowPath> implements FollowPathDao {
+    @Override
+    public void removeFollowPath(String spiderName) {
+        Query query = new Query();
+        query.addCriteria(new Criteria("spider_name").is(spiderName));
+        this.remove(query, FollowPath.class);
+    }
+}
